10a6c0007ce16fd624d46b2e3f4463b74bd8377f
cotto89 <cottto89@gmail.com>
2016-09-04 19:36:45 +0900

    add: mixin.js

---
+++ src/helper/mixin.js
@@ -0,0 +1,33 @@
+import isFunction from 'lodash.isfunction';
+import isPlainObject from 'lodash.isplainobject';
+import toPairs from 'lodash.topairs';
+
+/**
+ * Merge object and auto bind this context
+ *
+ * @param {Object} master
+ * @param {Object} src
+ * @param {Object} context
+ * @param {String[]} ignore
+ * @example mixin.call(this, src, this, ['ignore', 'props']);
+ */
+export default function mixin(master, src = {}, context = {}, ignore = []) {
+    for (const [k, v] of toPairs(src)) {
+        if (ignore.includes(k)) continue;
+
+        if (isPlainObject(v)) {
+            if (!context[k]) context[k] = {};
+            mixin(master, v, context[k], ignore);
+            continue;
+        }
+
+        if (isFunction(v)) {
+            context[k] = v.bind(master);
+            continue;
+        }
+
+        context[k] = v;
+    }
+
+    return context;
+}
---
+++ test/helper/mixin.test.js
@@ -0,0 +1,77 @@
+import assert from 'power-assert';
+import mixin from './../../src/helper/mixin.js';
+
+/* mock */
+class Counter {
+    constructor() {
+        this.state = { count: 0 };
+    }
+}
+
+const src = {
+    state: { count: 100 },
+    str: 'some text',
+
+    setState(nextState) {
+        this.state = Object.assign({}, this.state, nextState);
+    },
+    actions: {
+        up() {
+            this.setState({ count: this.state.count + 1 });
+        },
+        down() {
+            this.setState({ count: this.state.count - 1 });
+        },
+
+        nest: {
+            clear() {
+                this.setState({ count: 0 });
+            },
+        },
+    },
+};
+
+
+describe('mixin()', function() {
+    beforeEach(function() {
+        this.src = src;
+        this.counter = new Counter();
+    });
+
+    it('merge deeply properties of counter and src', function() {
+        const ret = mixin(this.counter, this.src, this.counter, ['state']);
+        assert(ret instanceof Counter);
+        assert(this.counter instanceof Counter);
+        assert.deepStrictEqual(ret, this.counter);
+
+        const { state, str, actions } = this.counter;
+        assert(!!state);
+        assert(!!str);
+        assert(!!actions);
+        assert(!!actions.up);
+        assert(!!actions.down);
+        assert(!!actions.nest);
+        assert(!!actions.nest.clear);
+    });
+
+    it('ignore specific properties', function() {
+        mixin(this.counter, this.src, this.counter, ['state']);
+        assert.notDeepStrictEqual(this.counter.state, this.src.state);
+        assert.deepStrictEqual(this.counter.state, { count: 0 });
+    });
+
+    it('is bound this when functional properties', function() {
+        mixin(this.counter, this.src, this.counter, ['state']);
+
+        this.counter.actions.up();
+        assert.deepStrictEqual(this.counter.state, { count: 1 });
+
+        for (let i = 0; i < 3; i++) {
+            this.counter.actions.down();
+        }
+        assert.deepStrictEqual(this.counter.state, { count: -2 });
+
+        this.counter.actions.nest.clear();
+        assert.deepStrictEqual(this.counter.state, { count: 0 });
+    });
+});
