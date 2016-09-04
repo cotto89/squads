332693dd018acdf826a6ecb0d0a600959f751c9a
cotto89 <cottto89@gmail.com>
2016-09-04 22:34:51 +0900

    add: build.js

---
+++ src/lib/build.js
@@ -0,0 +1,42 @@
+import ActionEmitter from './ActionEmitter.js';
+import StateDispatcher from './StateDispatcher.js';
+
+export const emitter = new ActionEmitter();
+export const dispatcher = new StateDispatcher();
+export const dispatch = emitter.dispatch;
+
+/**
+ * Build Squads
+ *
+ * @param {Object} options
+ * @param {Squad[]} options.squads
+ */
+export default function build(options) {
+    const { squads } = options;
+
+    /* Connect squads to ActionEmitter */
+    for (const squad of squads) {
+        squad.__connect__(emitter, dispatcher);
+    }
+
+    function getState() {
+        const state = {};
+        for (const squad of squads) {
+            state[squad.context] = squad.state;
+        }
+        return state;
+    }
+
+    /**
+     * @param {Function} handler
+     */
+    function onChange(handler) {
+        dispatcher.on('state:change', handler);
+    }
+
+    return {
+        getState,
+        onChange,
+        dispatch: emitter.dispatch,
+    };
+}
