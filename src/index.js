b4e091cfaca71ea6aadafce071d59bc936f6dff7
cotto89 <cottto89@gmail.com>
2016-09-14 10:29:48 +0900

    modify:  index.js
    テストやdebugのためにindex.jsにActionEmitterとStateDispatcherのinstanceを含めるように変更。

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import Squad from './lib/Squad.js';
 import SharedAction from './lib/SharedAction.js';
 import store from './lib/store.js';
-import { dispatch } from './lib/ActionEmitter.js';
+import actionEmitter, { dispatch } from './lib/ActionEmitter.js';
+import stateDispatcher from './lib/StateDispatcher.js';
 
 module.exports = {
     store,
     dispatch,
     Squad,
-    SharedAction
+    SharedAction,
+    actionEmitter,
+    stateDispatcher
 };
